cb034a1d28c43a9374c3adc1f6a0acee56d8fbdb
mmn1998 <mm.nomani.1998@gmail.com>
2025-01-26 13:24:04 +0330

    id bug in mappers fixed

--- Src/Application/SIMA.Application/Feaatures/RiskManagers/ConsequenceLevels/Mappers/ConsequenceLevelMapper.cs
+++ Src/Application/SIMA.Application/Feaatures/RiskManagers/ConsequenceLevels/Mappers/ConsequenceLevelMapper.cs
@@ -12,12 +12,11 @@ public class ConsequenceLevelMapper : Profile
     {
         CreateMap<CreateConsequenceLevelCommand, CreateConsequenceLevelArg>()
             .ForMember(dest => dest.ActiveStatusId, act => act.MapFrom(source => (long)ActiveStatusEnum.Active))
-            //.ForMember(dest => dest.CreatedBy, act => act.MapFrom(source => simaIdentity.UserId))
+            .ForMember(dest => dest.Id, act => act.MapFrom(source => IdHelper.GenerateUniqueId()))
             .ForMember(dest => dest.CreatedAt, act => act.MapFrom(source => DateTime.Now));
 
         CreateMap<ModifyConsequenceLevelCommand, ModifyConsequenceLevelArg>()
             .ForMember(dest => dest.ActiveStatusId, act => act.MapFrom(source => (long)ActiveStatusEnum.Active))
-            //.ForMember(dest => dest.ModifyBy, act => act.MapFrom(source => simaIdentity.UserId))
             .ForMember(dest => dest.ModifiedAt, act => act.MapFrom(source => Encoding.UTF8.GetBytes(DateTime.Now.ToString())));
     }
 }
