58abe1bfb2300f6d4beeaa41dccd802d0d76a967
mmn1998 <mm.nomani.1998@gmail.com>
2025-01-27 12:04:02 +0330

    InherentOccurrenceProbability Crud

---
+++ Src/Application/SIMA.Application.Contract/Features/RiskManagers/CurrentOccurrenceProbabilities/CreateCurrentOccurrenceProbabilityCommand.cs
@@ -0,0 +1,12 @@
+﻿using SIMA.Framework.Common.Response;
+using Sima.Framework.Core.Mediator;
+
+namespace SIMA.Application.Contract.Features.RiskManagers.CurrentOccurrenceProbabilities;
+
+public class CreateCurrentOccurrenceProbabilityCommand : ICommand<Result<long>>
+{
+    public string Code { get; set; }
+    public long MatrixAValueId { get; set; }
+    public long InherentOccurrenceProbabilityValueId { get; set; }
+    public long ScenarioHistoryId { get; set; }
+}
---
+++ Src/Application/SIMA.Application/Feaatures/RiskManagers/CurrentOccurrenceProbabilities/Mappers/CurrentOccurrenceProbabilityMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SIMA.Application.Contract.Features.RiskManagers.CurrentOccurrenceProbabilities;
+using SIMA.Domain.Models.Features.RiskManagement.CurrentOccurrenceProbabilities.Args;
+using SIMA.Framework.Common.Helper;
+using System.Text;
+
+namespace SIMA.Application.Feaatures.RiskManagers.CurrentOccurrenceProbabilities.Mappers;
+
+public class CurrentOccurrenceProbabilityMapper : Profile
+{
+    public CurrentOccurrenceProbabilityMapper()
+    {
+        CreateMap<CreateCurrentOccurrenceProbabilityCommand, CreateCurrentOccurrenceProbabilityArg>()
+            .ForMember(dest => dest.ActiveStatusId, act => act.MapFrom(source => (long)ActiveStatusEnum.Active))
+            .ForMember(dest => dest.Id, act => act.MapFrom(source => IdHelper.GenerateUniqueId()))
+            .ForMember(dest => dest.CreatedAt, act => act.MapFrom(source => DateTime.Now));
+
+        CreateMap<ModifyCurrentOccurrenceProbabilityCommand, ModifyCurrentOccurrenceProbabilityArg>()
+            .ForMember(dest => dest.ActiveStatusId, act => act.MapFrom(source => (long)ActiveStatusEnum.Active))
+            .ForMember(dest => dest.ModifiedAt, act => act.MapFrom(source => Encoding.UTF8.GetBytes(DateTime.Now.ToString())));
+    }
+}
