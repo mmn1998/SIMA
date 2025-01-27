25784bd51b5f85b2163dc850e818cc123632e72f
Radmehr314 <133379613+Radmehr314@users.noreply.github.com>
2025-01-27 09:04:04 +0330

    fix sql query bugs for TriggerStatus and UseVulnerability

--- Src/Infrastructure/SIMA.Persistance.Read/Repositories/Features/RiskManagement/TriggerStatuses/TriggerStatusQueryRepository.cs
+++ Src/Infrastructure/SIMA.Persistance.Read/Repositories/Features/RiskManagement/TriggerStatuses/TriggerStatusQueryRepository.cs
@@ -1,39 +1,43 @@
-﻿using ArmanIT.Investigation.Dapper.QueryBuilder;
+﻿using System.Data.SqlClient;
+using ArmanIT.Investigation.Dapper.QueryBuilder;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using SIMA.Application.Query.Contract.Features.RiskManagement.ThreatTypes;
-using SIMA.Application.Query.Contract.Features.RiskManagement.TriggerStatuses;
 using SIMA.Framework.Common.Exceptions;
 using SIMA.Framework.Common.Helper;
 using SIMA.Framework.Common.Response;
-using System.Data.SqlClient;
+using SIMA.Persistance.Read.Repositories.Features.RiskManagement.ThreatTypes;
 
 namespace SIMA.Persistance.Read.Repositories.Features.RiskManagement.TriggerStatuses;
 
-public class TriggerStatusQueryRepository : ITriggerStatusQueryRepository
+public class TriggerStatusQueryRepository: IThreatTypeQueryRepository
 {
     private readonly string _connectionString;
     private readonly string _mainQuery;
 
     public TriggerStatusQueryRepository(IConfiguration configuration)
     {
-        _connectionString = configuration.GetConnectionString();
+        _connectionString = configuration.GetConnectionString();;
+        //ToDo Fix From Name
         _mainQuery = @"SELECT DISTINCT T.[Id]
                       ,T.[Name]
                       ,T.[Code]
-                      ,T.[NumericValue]
                       ,T.[ActiveStatusId]
+					  ,T.[NumericValue]
+					  ,T.[ValueTitle]
                       ,T.[CreatedAt]
 	                  , S.[Name] as ActiveStatus
                   FROM [RiskManagement].[TriggerStatus] T
+				  INNER JOIN [Basic].[ActiveStatus] S on S.ID = T.ActiveStatusId
                   WHERE T.ActiveStatusId != 3";
     }
-    public async Task<Result<IEnumerable<GetTriggerStatusesQueryResult>>> GetAll(GetAllTriggerStatusesQuery request)
+    public async Task<Result<IEnumerable<GetThreatTypesQueryResult>>> GetAll(GetAllThreatTypesQuery request)
     {
-        using var connection = new SqlConnection(_connectionString);
-        await connection.OpenAsync();
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
 
-        string queryCount = $@" WITH Query as(
+            string queryCount = $@" WITH Query as(
 						                    {_mainQuery}
 							)
 								SELECT Count(*) FROM Query
@@ -42,30 +46,40 @@ public class TriggerStatusQueryRepository : ITriggerStatusQueryRepository
 								 ; ";
 
 
-        string query = $@" WITH Query as(
+            string query = $@" WITH Query as(
 							                  {_mainQuery}
 							)
 								SELECT * FROM Query
 								 /**where**/
 								 /**orderby**/
                                     OFFSET @Skip rows FETCH NEXT @PageSize rows only; ";
-        var dynaimcParameters = DapperHelperExtention.GenerateQuery(queryCount + query, request);
-        using var multi = await connection.QueryMultipleAsync(dynaimcParameters.Item1.RawSql, dynaimcParameters.Item2);
-        var count = await multi.ReadFirstAsync<int>();
-        var response = await multi.ReadAsync<GetTriggerStatusesQueryResult>();
-        return Result.Ok(response, request, count);
+            var dynaimcParameters = DapperHelperExtention.GenerateQuery(queryCount + query, request);
+            using (var multi = await connection.QueryMultipleAsync(dynaimcParameters.Item1.RawSql, dynaimcParameters.Item2))
+            {
+                var count = await multi.ReadFirstAsync<int>();
+                var response = await multi.ReadAsync<GetThreatTypesQueryResult>();
+                return Result.Ok(response, request, count);
+            }
+        }
     }
 
-    public async Task<GetTriggerStatusesQueryResult> GetById(long id)
+    public async Task<GetThreatTypesQueryResult> GetById(long id)
     {
-        using var connection = new SqlConnection(_connectionString);
-        await connection.OpenAsync();
-        string query = $@"
-              {_mainQuery}
-                  AND T.Id = @Id ";
-        var result = await connection.QueryFirstOrDefaultAsync<GetTriggerStatusesQueryResult>(query, new { Id = id });
-        result.NullCheck();
-        return result ?? throw SimaResultException.NotFound;
-
+	    using (var connection = new SqlConnection(_connectionString))
+	    {
+		    await connection.OpenAsync();
+		    string query = @"
+              SELECT DISTINCT T.[Id]
+                      ,T.[Name]
+                      ,T.[Code]
+                      ,T.[ActiveStatusId]
+	                  , S.[Name] as ActiveStatus
+                  FROM [RiskManagement].[ThreatType] T
+                  INNER JOIN [Basic].[ActiveStatus] S on S.ID = T.ActiveStatusId
+                  WHERE T.Id = @Id and T.ActiveStatusId != 3";
+		    var result = await connection.QueryFirstOrDefaultAsync<GetThreatTypesQueryResult>(query, new { Id = id });
+		    result.NullCheck();
+		    return result ?? throw SimaResultException.NotFound;
+	    }
     }
 }
