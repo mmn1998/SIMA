fa3811cf99c618aaef54a21ea1f486851d48ad49
mmn1998 <mm.nomani.1998@gmail.com>
2025-02-02 10:06:12 +0330

    migrated to database

--- Src/Infrastructure/SIMA.Persistance/Migrations/20250202063201_cobit20250202.Designer.cs
+++ Src/Infrastructure/SIMA.Persistance/Migrations/20250202063201_cobit20250202.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SIMA.Persistance.Persistence;
 
@@ -11,9 +12,11 @@ using SIMA.Persistance.Persistence;
 namespace SIMA.Persistance.Migrations
 {
     [DbContext(typeof(SIMADBContext))]
-    partial class SIMADBContextModelSnapshot : ModelSnapshot
+    [Migration("20250202063201_cobit20250202")]
+    partial class cobit20250202
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
